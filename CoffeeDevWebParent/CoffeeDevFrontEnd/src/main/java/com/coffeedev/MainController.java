99109054a57ab011eb5b3b7ba66f69afb2d14843
vodinhluan <luandinhvo2002@gmail.com>
2023-10-20 16:14:35 +0700

    front end part 1

--- CoffeeDevWebParent/CoffeeDevFrontEnd/src/main/java/com/coffeedev/MainController.java
+++ CoffeeDevWebParent/CoffeeDevFrontEnd/src/main/java/com/coffeedev/MainController.java
@@ -5,8 +5,56 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-	@GetMapping("")
+	@GetMapping("/index.html")
 	public String viewHomePage() {
 		return "index";
 	}
+	
+	@GetMapping("/menu.html")
+	public String viewMenu() {
+		return "menu";
+	}
+	
+	@GetMapping("/services.html")
+	public String viewService() {
+		return "services";
+	}
+	
+	@GetMapping("/cart.html")
+	public String viewCart() {
+		return "cart";
+	}
+	
+	@GetMapping("/blog.html")
+	public String viewBlog() {
+		return "blog";
+	}
+	
+	@GetMapping("/about.html")
+	public String viewAbout() {
+		return "about";
+	}
+	
+	@GetMapping("/checkout.html")
+	public String viewCheckout() {
+		return "checkout";
+	}
+	
+	@GetMapping("/contact.html")
+	public String viewContact() {
+		return "contact";
+	}
+	
+	@GetMapping("/shop.html")
+	public String viewShop() {
+		return "shop";
+	}
+	
+	@GetMapping("/product-single.html")
+	public String viewProductSingle() {
+		return "product-single";
+	}
+	
+	
+	
 }
