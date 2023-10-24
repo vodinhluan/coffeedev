7da540c31c092b2cc041907a0cc2e4963e80ceb1
vodinhluan <luandinhvo2002@gmail.com>
2023-10-24 17:55:06 +0700

    Code repository layer for customer

---
+++ CoffeeDevCommon/src/main/java/com/coffeedev/common/entity/Customer.java
@@ -0,0 +1,123 @@
+package com.coffeedev.common.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false, unique = true, length = 45)
+	private String email;
+
+	@Column(nullable = false, length = 64)
+	private String password;
+
+	@Column(name = "name", nullable = false, length = 45)
+	private String name;
+
+	@Column(name = "phone_number", nullable = false, length = 15)
+	private String phoneNumber;
+
+	@Column(name = "address", length = 64)
+	private String address;
+
+	@Column(name = "verification_code", length = 64)
+	private String verificationCode;
+
+	private boolean enabled;
+
+	@Column(name = "created_time")
+	private Date createdTime;
+
+	public Customer() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getVerificationCode() {
+		return verificationCode;
+	}
+
+	public void setVerificationCode(String verificationCode) {
+		this.verificationCode = verificationCode;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Date getCreatedTime() {
+		return createdTime;
+	}
+
+	public void setCreatedTime(Date createdTime) {
+		this.createdTime = createdTime;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", email=" + email + ", Name=" + name + "]";
+	}
+
+}
---
+++ CoffeeDevWebParent/CoffeeDevBackend/src/test/java/com/coffeedev/admin/user/CustomerRepositoryTests.java
@@ -0,0 +1,113 @@
+package com.coffeedev.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Date;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import com.coffeedev.admin.customer.CustomerRepository;
+import com.coffeedev.common.entity.Customer;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class CustomerRepositoryTests {
+
+	@Autowired private CustomerRepository repo;
+	@Autowired private TestEntityManager entityManager;
+
+	@Test
+	public void testCreateCustomer1() {
+
+		Customer customer = new Customer();
+		customer.setName("Luan");
+		customer.setPassword("vodinhluan123");
+		customer.setEmail("luandinhvo2002@gmail.com.com");
+		customer.setPhoneNumber("312-462-7518");
+		customer.setAddress("1927  West Drive");
+
+		customer.setCreatedTime(new Date());
+
+		Customer savedCustomer = repo.save(customer);
+
+		assertThat(savedCustomer).isNotNull();
+		assertThat(savedCustomer.getId()).isGreaterThan(0);
+	}
+
+	
+
+	@Test
+	public void testListCustomers() {
+		Iterable<Customer> customers = repo.findAll();
+		customers.forEach(System.out::println);
+
+		assertThat(customers).hasSizeGreaterThan(1);
+	}
+
+	@Test
+	public void testUpdateCustomer() {
+		Integer customerId = 1;
+		String name = "Stanfield";
+
+		Customer customer = repo.findById(customerId).get();
+		customer.setName(name);
+		customer.setEnabled(true);
+
+		Customer updatedCustomer = repo.save(customer);
+		assertThat(updatedCustomer.getName()).isEqualTo(name);
+	}
+
+	@Test
+	public void testGetCustomer() {
+		Integer customerId = 2;
+		Optional<Customer> findById = repo.findById(customerId);
+
+		assertThat(findById).isPresent();
+
+		Customer customer = findById.get();
+		System.out.println(customer);
+	}
+
+	@Test
+	public void testDeleteCustomer() {
+		Integer customerId = 2;
+		repo.deleteById(customerId);
+
+		Optional<Customer> findById = repo.findById(customerId);		
+		assertThat(findById).isNotPresent();		
+	}
+
+	@Test
+	public void testFindByEmail() {
+		String email = "david.s.fountaine@gmail.com";
+		Customer customer = repo.findByEmail(email);
+
+		assertThat(customer).isNotNull();
+		System.out.println(customer);		
+	}
+
+	@Test
+	public void testFindByVerificationCode() {
+		String code = "code_123";
+		Customer customer = repo.findByVerificationCode(code);
+
+		assertThat(customer).isNotNull();
+		System.out.println(customer);		
+	}
+
+	@Test
+	public void testEnableCustomer() {
+		Integer customerId = 1;
+		repo.enable(customerId);
+
+		Customer customer = repo.findById(customerId).get();
+		assertThat(customer.isEnabled()).isTrue();
+	}
+}
