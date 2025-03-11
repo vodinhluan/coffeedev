4577d68a59812d1cdd96a076a3bb9b1e89852e3d
vodinhluan <luandinhvo2002@gmail.com>
2025-03-11 14:58:55 +0700

    refactor frontend reactjs

--- CoffeeDevWebParent/CoffeeDevBackend/src/main/java/com/coffeedev/admin/customer/CustomerRestController.java
+++ CoffeeDevWebParent/CoffeeDevBackend/src/main/java/com/coffeedev/admin/customer/CustomerRestController.java
@@ -7,7 +7,6 @@ import com.coffeedev.common.mapper.CustomerMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
