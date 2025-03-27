31c90df7b2ecc274201fb5d517f4fc02ac4c56c1
vodinhluan <luandinhvo2002@gmail.com>
2025-03-27 17:05:53 +0700

    Refactor CheckoutController and OrderService for improved order processing logic

--- CoffeeDevWebParent/CoffeeDevFrontEnd/src/main/java/com/coffeedev/checkout/CheckoutController.java
+++ CoffeeDevWebParent/CoffeeDevFrontEnd/src/main/java/com/coffeedev/checkout/CheckoutController.java
@@ -1,8 +1,6 @@
 package com.coffeedev.checkout;
 
-import java.io.UnsupportedEncodingException;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,22 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.coffeedev.common.entity.CartItem;
 import com.coffeedev.common.entity.Customer;
 import com.coffeedev.common.entity.District;
-import com.coffeedev.common.entity.Order;
 import com.coffeedev.common.entity.PaymentMethod;
-import com.coffeedev.common.entity.ShippingRate;
 import com.coffeedev.customer.CustomerService;
 import com.coffeedev.order.OrderService;
 import com.coffeedev.setting.Utilities;
 import com.coffeedev.shoppingcart.ShoppingCartService;
 
-import jakarta.mail.MessagingException;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class CheckoutController {
@@ -65,7 +58,7 @@ public class CheckoutController {
 	
 	
 	@PostMapping("/checkout/submit")
-	public String placeOrder(@ModelAttribute("customer") Customer customer,
+	public String placeOrder(@ModelAttribute Customer customer,
 			@RequestParam("quan") String district,
 			@RequestParam("paymentMethod") String payment,
 	                         HttpServletRequest request,
